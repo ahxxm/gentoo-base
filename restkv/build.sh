6f6390c7f20a9ca4528e22de46a5a8dc14e4b946
ahxxm <i@ahxxm.com>
2020-12-06 14:11:41 +0800

    use relative path

--- restkv/build.sh
+++ restkv/build.sh
@@ -2,13 +2,10 @@
 
 set -ex
 
-mkdir -p /kv
-cd /kv
-
 sudo apt update && sudo apt install -y git
 git clone https://github.com/ahxxm/restkv
 cd restkv
 git checkout 1a816979faa42dc1f96fb04f025e29b4203cbbf9
 cargo build --release --target x86_64-unknown-linux-musl
 
-cp target/x86_64-unknown-linux-musl/release/restkv /kv/
+sudo cp target/x86_64-unknown-linux-musl/release/restkv /
